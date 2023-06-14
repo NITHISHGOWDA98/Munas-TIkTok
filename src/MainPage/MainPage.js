4cb9be7a674be2da83749ce5156eaa6a79648cc0
NITHISHGOWDA98 <nithishgowda98@gmail.com>
2023-06-14 10:35:37 +0530

    added transition

--- src/MainPage/MainPage.js
+++ src/MainPage/MainPage.js
@@ -5,25 +5,43 @@ import img1 from "../assets/img1.jpeg";
 import img2 from "../assets/img2.jpeg";
 import img3 from "../assets/img3.jpeg";
 import img4 from "../assets/img4.jpeg";
+
 function MainPage() {
   return (
     <div className="MainPage">
       <Header />
-      <div className=" MainPagecontainer">
+      <div className="MainPagecontainer">
         <div className="box1">
-          <img src={img1} alt="1" />
-          {/* <div className="desc">
-            <h1>GYM WEAR</h1>
-          </div> */}
+          <div className="imgd">
+            <img src={img1} alt="1" />
+          </div>
+          <div className="box-description">
+            <b>GYM WEAR</b>
+          </div>
         </div>
         <div className="box2">
-          <img src={img2} alt="1" />
+          <div className="imgd">
+            <img src={img2} alt="2" />
+          </div>
+          <div className="box-description">
+            <b>SUMMER WEAR</b>
+          </div>
         </div>
         <div className="box3">
-          <img src={img3} alt="1" />
+          <div className="imgd">
+            <img src={img3} alt="3" />
+          </div>
+          <div className="box-description">
+            <b>CASUAL WEAR</b>
+          </div>
         </div>
         <div className="box4">
-          <img src={img4} alt="1" />
+          <div className="imgd">
+            <img src={img4} alt="4" />
+          </div>
+          <div className="box-description">
+            <b>PARTY WEAR</b>
+          </div>
         </div>
       </div>
     </div>
