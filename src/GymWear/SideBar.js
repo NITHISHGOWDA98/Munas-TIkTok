cbe932d683f7dd3b7dccb7b0c57f22e159ddf648
NITHISHGOWDA98 <121144422+NITHISHGOWDA98@users.noreply.github.com>
2023-06-15 18:06:07 +0530

    ready to deploy

--- src/GymWear/SideBar.js
+++ src/GymWear/SideBar.js
@@ -1,17 +1,4 @@
 import React from "react";
-<<<<<<< HEAD
-import "./sidebar.css";
-export const SideBar = () => {
-  return (
-    <>
-      <div className="sidebarmaincontent">
-        <div className="sidebarheader">
-        Filter
-        </div>
-        
-        </div>
-    </>
-=======
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 import { faFilter } from "@fortawesome/free-solid-svg-icons";
 import "./sidebar.css";
@@ -29,6 +16,7 @@ export const SideBar = () => {
       </div>
       
     </div>
->>>>>>> 79954b9605fb8eb560a7565d2a917d230c8da80b
   );
+
+
 };
