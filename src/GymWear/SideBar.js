0e619ca28df8c2a95a976e70e2ce67e029ebee58
2amlover <126642452+2amlover@users.noreply.github.com>
2023-06-15 16:58:13 +0530

    adedd filter option in sidebar

--- src/GymWear/SideBar.js
+++ src/GymWear/SideBar.js
@@ -1,9 +1,20 @@
 import React from "react";
-import "./sidebar.css"
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import { faFilter } from "@fortawesome/free-solid-svg-icons";
+import "./sidebar.css";
+
 export const SideBar = () => {
   return (
-
-      <div className="sidebarmaincontent">SideBar</div>
-    
+    <div className="sidebarmaincontent">
+      <div className="headerfilter">
+        <div className="filtericon">
+          <FontAwesomeIcon icon={faFilter} />
+        </div>
+        <div className="filtertext">
+          <h2>FILTER</h2>
+        </div>
+      </div>
+      
+    </div>
   );
 };
