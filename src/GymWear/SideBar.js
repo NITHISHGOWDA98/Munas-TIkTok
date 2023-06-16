577f0d51a28ece0694d435bd59e403bc1c189e1a
2amlover <126642452+2amlover@users.noreply.github.com>
2023-06-16 12:31:49 +0530

    side bar

--- src/GymWear/SideBar.js
+++ src/GymWear/SideBar.js
@@ -5,6 +5,7 @@ import "./sidebar.css";
 
 export const SideBar = () => {
   return (
+    <>
     <div className="sidebarmaincontent">
       <div className="headerfilter">
         <div className="filtericon">
@@ -14,7 +15,47 @@ export const SideBar = () => {
           <h2>FILTER</h2>
         </div>
       </div>
-      
+      <div className="options">
+        <div className="option">
+          <input type="checkbox" id="tshirts" name="tshirts" />
+          <label htmlFor="tshirts">T-shirts</label>
+        </div>
+        <div className="option">
+          <input type="checkbox" id="trousers" name="trousers" />
+          <label htmlFor="trousers">Trousers and Tights</label>
+        </div>
+        <div className="option">
+          <input type="checkbox" id="tracksuits" name="tracksuits" />
+          <label htmlFor="tracksuits">Track-suits</label>
+        </div>
+        <div className="option">
+          <input type="checkbox" id="shorts" name="shorts" />
+          <label htmlFor="shorts">Shorts</label>
+        </div>
+      </div>
+      <div className="size">
+        <h2>Size</h2>
+        <div className="option">
+          <input type="checkbox" id="size-s" name="size-s" />
+          <label htmlFor="size-s">Small</label>
+        </div>
+        <div className="option">
+          <input type="checkbox" id="size-m" name="size-m" />
+          <label htmlFor="size-m">Medium</label>
+        </div>
+        <div className="option">
+          <input type="checkbox" id="size-l" name="size-l" />
+          <label htmlFor="size-l">Large</label>
+        </div>
+        <div className="option">
+          <input type="checkbox" id="size-xl" name="size-xl" />
+          <label htmlFor="size-xl">Extra Large</label>
+        </div>
+      </div>
+      <div className="price">
+        <h2>Price</h2>
+        </div>
     </div>
+    </>
   );
 };
