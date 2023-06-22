d10c5f8be5b2632d81b8be2d2c4c7450f821291d
NITHISHGOWDA98 <121144422+NITHISHGOWDA98@users.noreply.github.com>
2023-06-22 10:54:05 +0530

    version 1

--- src/GymWear/GymWear.js
+++ src/GymWear/GymWear.js
@@ -34,8 +34,8 @@ export const GymWear = () => {
         "https://static.nike.com/a/images/c_limit,w_592,f_auto/t_product_v1/i1-2ad24be7-f94e-49d0-8f4a-aa39a15fae36/pro-mens-training-shorts-v2CKMc.png"
     },
     // Add more clothing objects as needed
-  ];
-  const clothingList2 = [
+  
+  
     {
       id: 1,
       name: "Nike Dri-FIT Men's Training T-Shirt",
@@ -64,16 +64,339 @@ export const GymWear = () => {
       imageUrl:
         "https://static.nike.com/a/images/c_limit,w_592,f_auto/t_product_v1/i1-2ad24be7-f94e-49d0-8f4a-aa39a15fae36/pro-mens-training-shorts-v2CKMc.png"
     },
+    {
+      id: 1,
+      name: "Nike Dri-FIT Men's Training T-Shirt",
+      price: "700",
+      imageUrl:
+        "https://static.nike.com/a/images/c_limit,w_592,f_auto/t_product_v1/8ab0abdd-4cc2-47f5-883e-eb4a902c7068/dri-fit-mens-training-t-shirt-ZMzCRw.png"
+    },
+    {
+      id: 2,
+      name: "Nike Pro Men's Training Shorts",
+      price: "705",
+      imageUrl:
+        "https://static.nike.com/a/images/c_limit,w_592,f_auto/t_product_v1/i1-2ad24be7-f94e-49d0-8f4a-aa39a15fae36/pro-mens-training-shorts-v2CKMc.png"
+    },
+    {
+      id: 3,
+      name: "Nike Dri-FIT Men's Training T-Shirt",
+      price: "700",
+      imageUrl:
+        "https://static.nike.com/a/images/c_limit,w_592,f_auto/t_product_v1/8ab0abdd-4cc2-47f5-883e-eb4a902c7068/dri-fit-mens-training-t-shirt-ZMzCRw.png"
+    },
+    {
+      id: 4,
+      name: "Nike Pro Men's Training Shorts",
+      price: "705",
+      imageUrl:
+        "https://static.nike.com/a/images/c_limit,w_592,f_auto/t_product_v1/i1-2ad24be7-f94e-49d0-8f4a-aa39a15fae36/pro-mens-training-shorts-v2CKMc.png"
+    },
+    // Add more clothing objects as needed
+  
+    {
+      id: 1,
+      name: "Nike Dri-FIT Men's Training T-Shirt",
+      price: "7000",
+      imageUrl:
+        "https://static.nike.com/a/images/c_limit,w_592,f_auto/t_product_v1/8ab0abdd-4cc2-47f5-883e-eb4a902c7068/dri-fit-mens-training-t-shirt-ZMzCRw.png"
+    },
+    {
+      id: 2,
+      name: "Nike Pro Men's Training Shorts",
+      price: "705",
+      imageUrl:
+        "https://static.nike.com/a/images/c_limit,w_592,f_auto/t_product_v1/i1-2ad24be7-f94e-49d0-8f4a-aa39a15fae36/pro-mens-training-shorts-v2CKMc.png"
+    },
+    {
+      id: 3,
+      name: "Nike Dri-FIT Men's Training T-Shirt",
+      price: "700",
+      imageUrl:
+        "https://static.nike.com/a/images/c_limit,w_592,f_auto/t_product_v1/8ab0abdd-4cc2-47f5-883e-eb4a902c7068/dri-fit-mens-training-t-shirt-ZMzCRw.png"
+    },
+    {
+      id: 4,
+      name: "Nike Pro Men's Training Shorts",
+      price: "705",
+      imageUrl:
+        "https://static.nike.com/a/images/c_limit,w_592,f_auto/t_product_v1/i1-2ad24be7-f94e-49d0-8f4a-aa39a15fae36/pro-mens-training-shorts-v2CKMc.png"
+    },
+    {
+      id: 1,
+      name: "Nike Dri-FIT Men's Training T-Shirt",
+      price: "7000",
+      imageUrl:
+        "https://static.nike.com/a/images/c_limit,w_592,f_auto/t_product_v1/8ab0abdd-4cc2-47f5-883e-eb4a902c7068/dri-fit-mens-training-t-shirt-ZMzCRw.png"
+    },
+    {
+      id: 2,
+      name: "Nike Pro Men's Training Shorts",
+      price: "705",
+      imageUrl:
+        "https://static.nike.com/a/images/c_limit,w_592,f_auto/t_product_v1/i1-2ad24be7-f94e-49d0-8f4a-aa39a15fae36/pro-mens-training-shorts-v2CKMc.png"
+    },
+    {
+      id: 3,
+      name: "Nike Dri-FIT Men's Training T-Shirt",
+      price: "700",
+      imageUrl:
+        "https://static.nike.com/a/images/c_limit,w_592,f_auto/t_product_v1/8ab0abdd-4cc2-47f5-883e-eb4a902c7068/dri-fit-mens-training-t-shirt-ZMzCRw.png"
+    },
+    {
+      id: 4,
+      name: "Nike Pro Men's Training Shorts",
+      price: "705",
+      imageUrl:
+        "https://static.nike.com/a/images/c_limit,w_592,f_auto/t_product_v1/i1-2ad24be7-f94e-49d0-8f4a-aa39a15fae36/pro-mens-training-shorts-v2CKMc.png"
+    },
+    {
+      id: 1,
+      name: "Nike Dri-FIT Men's Training T-Shirt",
+      price: "700",
+      imageUrl:
+        "https://static.nike.com/a/images/c_limit,w_592,f_auto/t_product_v1/8ab0abdd-4cc2-47f5-883e-eb4a902c7068/dri-fit-mens-training-t-shirt-ZMzCRw.png"
+    },
+    {
+      id: 2,
+      name: "Nike Pro Men's Training Shorts",
+      price: "705",
+      imageUrl:
+        "https://static.nike.com/a/images/c_limit,w_592,f_auto/t_product_v1/i1-2ad24be7-f94e-49d0-8f4a-aa39a15fae36/pro-mens-training-shorts-v2CKMc.png"
+    },
+    {
+      id: 3,
+      name: "Nike Dri-FIT Men's Training T-Shirt",
+      price: "700",
+      imageUrl:
+        "https://static.nike.com/a/images/c_limit,w_592,f_auto/t_product_v1/8ab0abdd-4cc2-47f5-883e-eb4a902c7068/dri-fit-mens-training-t-shirt-ZMzCRw.png"
+    },
+    {
+      id: 4,
+      name: "Nike Pro Men's Training Shorts",
+      price: "705",
+      imageUrl:
+        "https://static.nike.com/a/images/c_limit,w_592,f_auto/t_product_v1/i1-2ad24be7-f94e-49d0-8f4a-aa39a15fae36/pro-mens-training-shorts-v2CKMc.png"
+    },
+    // Add more clothing objects as needed
+  
+    {
+      id: 1,
+      name: "Nike Dri-FIT Men's Training T-Shirt",
+      price: "7000",
+      imageUrl:
+        "https://static.nike.com/a/images/c_limit,w_592,f_auto/t_product_v1/8ab0abdd-4cc2-47f5-883e-eb4a902c7068/dri-fit-mens-training-t-shirt-ZMzCRw.png"
+    },
+    {
+      id: 2,
+      name: "Nike Pro Men's Training Shorts",
+      price: "705",
+      imageUrl:
+        "https://static.nike.com/a/images/c_limit,w_592,f_auto/t_product_v1/i1-2ad24be7-f94e-49d0-8f4a-aa39a15fae36/pro-mens-training-shorts-v2CKMc.png"
+    },
+    {
+      id: 3,
+      name: "Nike Dri-FIT Men's Training T-Shirt",
+      price: "700",
+      imageUrl:
+        "https://static.nike.com/a/images/c_limit,w_592,f_auto/t_product_v1/8ab0abdd-4cc2-47f5-883e-eb4a902c7068/dri-fit-mens-training-t-shirt-ZMzCRw.png"
+    },
+    {
+      id: 4,
+      name: "Nike Pro Men's Training Shorts",
+      price: "705",
+      imageUrl:
+        "https://static.nike.com/a/images/c_limit,w_592,f_auto/t_product_v1/i1-2ad24be7-f94e-49d0-8f4a-aa39a15fae36/pro-mens-training-shorts-v2CKMc.png"
+    },
+  
+    {
+      id: 1,
+      name: "Nike Dri-FIT Men's Training T-Shirt",
+      price: "7000",
+      imageUrl:
+        "https://static.nike.com/a/images/c_limit,w_592,f_auto/t_product_v1/8ab0abdd-4cc2-47f5-883e-eb4a902c7068/dri-fit-mens-training-t-shirt-ZMzCRw.png"
+    },
+    {
+      id: 2,
+      name: "Nike Pro Men's Training Shorts",
+      price: "705",
+      imageUrl:
+        "https://static.nike.com/a/images/c_limit,w_592,f_auto/t_product_v1/i1-2ad24be7-f94e-49d0-8f4a-aa39a15fae36/pro-mens-training-shorts-v2CKMc.png"
+    },
+    {
+      id: 3,
+      name: "Nike Dri-FIT Men's Training T-Shirt",
+      price: "700",
+      imageUrl:
+        "https://static.nike.com/a/images/c_limit,w_592,f_auto/t_product_v1/8ab0abdd-4cc2-47f5-883e-eb4a902c7068/dri-fit-mens-training-t-shirt-ZMzCRw.png"
+    },
+    {
+      id: 4,
+      name: "Nike Pro Men's Training Shorts",
+      price: "705",
+      imageUrl:
+        "https://static.nike.com/a/images/c_limit,w_592,f_auto/t_product_v1/i1-2ad24be7-f94e-49d0-8f4a-aa39a15fae36/pro-mens-training-shorts-v2CKMc.png"
+    },
+    {
+      id: 1,
+      name: "Nike Dri-FIT Men's Training T-Shirt",
+      price: "700",
+      imageUrl:
+        "https://static.nike.com/a/images/c_limit,w_592,f_auto/t_product_v1/8ab0abdd-4cc2-47f5-883e-eb4a902c7068/dri-fit-mens-training-t-shirt-ZMzCRw.png"
+    },
+    {
+      id: 2,
+      name: "Nike Pro Men's Training Shorts",
+      price: "705",
+      imageUrl:
+        "https://static.nike.com/a/images/c_limit,w_592,f_auto/t_product_v1/i1-2ad24be7-f94e-49d0-8f4a-aa39a15fae36/pro-mens-training-shorts-v2CKMc.png"
+    },
+    {
+      id: 3,
+      name: "Nike Dri-FIT Men's Training T-Shirt",
+      price: "700",
+      imageUrl:
+        "https://static.nike.com/a/images/c_limit,w_592,f_auto/t_product_v1/8ab0abdd-4cc2-47f5-883e-eb4a902c7068/dri-fit-mens-training-t-shirt-ZMzCRw.png"
+    },
+    {
+      id: 4,
+      name: "Nike Pro Men's Training Shorts",
+      price: "705",
+      imageUrl:
+        "https://static.nike.com/a/images/c_limit,w_592,f_auto/t_product_v1/i1-2ad24be7-f94e-49d0-8f4a-aa39a15fae36/pro-mens-training-shorts-v2CKMc.png"
+    },
+    // Add more clothing objects as needed
+  
+    {
+      id: 1,
+      name: "Nike Dri-FIT Men's Training T-Shirt",
+      price: "7000",
+      imageUrl:
+        "https://static.nike.com/a/images/c_limit,w_592,f_auto/t_product_v1/8ab0abdd-4cc2-47f5-883e-eb4a902c7068/dri-fit-mens-training-t-shirt-ZMzCRw.png"
+    },
+    {
+      id: 2,
+      name: "Nike Pro Men's Training Shorts",
+      price: "705",
+      imageUrl:
+        "https://static.nike.com/a/images/c_limit,w_592,f_auto/t_product_v1/i1-2ad24be7-f94e-49d0-8f4a-aa39a15fae36/pro-mens-training-shorts-v2CKMc.png"
+    },
+    {
+      id: 3,
+      name: "Nike Dri-FIT Men's Training T-Shirt",
+      price: "700",
+      imageUrl:
+        "https://static.nike.com/a/images/c_limit,w_592,f_auto/t_product_v1/8ab0abdd-4cc2-47f5-883e-eb4a902c7068/dri-fit-mens-training-t-shirt-ZMzCRw.png"
+    },
+    {
+      id: 4,
+      name: "Nike Pro Men's Training Shorts",
+      price: "705",
+      imageUrl:
+        "https://static.nike.com/a/images/c_limit,w_592,f_auto/t_product_v1/i1-2ad24be7-f94e-49d0-8f4a-aa39a15fae36/pro-mens-training-shorts-v2CKMc.png"
+    },
+  
+    {
+      id: 1,
+      name: "Nike Dri-FIT Men's Training T-Shirt",
+      price: "7000",
+      imageUrl:
+        "https://static.nike.com/a/images/c_limit,w_592,f_auto/t_product_v1/8ab0abdd-4cc2-47f5-883e-eb4a902c7068/dri-fit-mens-training-t-shirt-ZMzCRw.png"
+    },
+    {
+      id: 2,
+      name: "Nike Pro Men's Training Shorts",
+      price: "705",
+      imageUrl:
+        "https://static.nike.com/a/images/c_limit,w_592,f_auto/t_product_v1/i1-2ad24be7-f94e-49d0-8f4a-aa39a15fae36/pro-mens-training-shorts-v2CKMc.png"
+    },
+    {
+      id: 3,
+      name: "Nike Dri-FIT Men's Training T-Shirt",
+      price: "700",
+      imageUrl:
+        "https://static.nike.com/a/images/c_limit,w_592,f_auto/t_product_v1/8ab0abdd-4cc2-47f5-883e-eb4a902c7068/dri-fit-mens-training-t-shirt-ZMzCRw.png"
+    },
+    {
+      id: 4,
+      name: "Nike Pro Men's Training Shorts",
+      price: "705",
+      imageUrl:
+        "https://static.nike.com/a/images/c_limit,w_592,f_auto/t_product_v1/i1-2ad24be7-f94e-49d0-8f4a-aa39a15fae36/pro-mens-training-shorts-v2CKMc.png"
+    },
+    {
+      id: 1,
+      name: "Nike Dri-FIT Men's Training T-Shirt",
+      price: "700",
+      imageUrl:
+        "https://static.nike.com/a/images/c_limit,w_592,f_auto/t_product_v1/8ab0abdd-4cc2-47f5-883e-eb4a902c7068/dri-fit-mens-training-t-shirt-ZMzCRw.png"
+    },
+    {
+      id: 2,
+      name: "Nike Pro Men's Training Shorts",
+      price: "705",
+      imageUrl:
+        "https://static.nike.com/a/images/c_limit,w_592,f_auto/t_product_v1/i1-2ad24be7-f94e-49d0-8f4a-aa39a15fae36/pro-mens-training-shorts-v2CKMc.png"
+    },
+    {
+      id: 3,
+      name: "Nike Dri-FIT Men's Training T-Shirt",
+      price: "700",
+      imageUrl:
+        "https://static.nike.com/a/images/c_limit,w_592,f_auto/t_product_v1/8ab0abdd-4cc2-47f5-883e-eb4a902c7068/dri-fit-mens-training-t-shirt-ZMzCRw.png"
+    },
+    {
+      id: 4,
+      name: "Nike Pro Men's Training Shorts",
+      price: "705",
+      imageUrl:
+        "https://static.nike.com/a/images/c_limit,w_592,f_auto/t_product_v1/i1-2ad24be7-f94e-49d0-8f4a-aa39a15fae36/pro-mens-training-shorts-v2CKMc.png"
+    },
+    // Add more clothing objects as needed
+  
+    {
+      id: 1,
+      name: "Nike Dri-FIT Men's Training T-Shirt",
+      price: "7000",
+      imageUrl:
+        "https://static.nike.com/a/images/c_limit,w_592,f_auto/t_product_v1/8ab0abdd-4cc2-47f5-883e-eb4a902c7068/dri-fit-mens-training-t-shirt-ZMzCRw.png"
+    },
+    {
+      id: 2,
+      name: "Nike Pro Men's Training Shorts",
+      price: "705",
+      imageUrl:
+        "https://static.nike.com/a/images/c_limit,w_592,f_auto/t_product_v1/i1-2ad24be7-f94e-49d0-8f4a-aa39a15fae36/pro-mens-training-shorts-v2CKMc.png"
+    },
+    {
+      id: 3,
+      name: "Nike Dri-FIT Men's Training T-Shirt",
+      price: "700",
+      imageUrl:
+        "https://static.nike.com/a/images/c_limit,w_592,f_auto/t_product_v1/8ab0abdd-4cc2-47f5-883e-eb4a902c7068/dri-fit-mens-training-t-shirt-ZMzCRw.png"
+    },
+    {
+      id: 4,
+      name: "Nike Pro Men's Training Shorts",
+      price: "705",
+      imageUrl:
+        "https://static.nike.com/a/images/c_limit,w_592,f_auto/t_product_v1/i1-2ad24be7-f94e-49d0-8f4a-aa39a15fae36/pro-mens-training-shorts-v2CKMc.png"
+    },
+  
+  
   ]
 
 
   return (
     <div>
-      <Header />
+     
 
       <div className="gymwear">
-        <SideBar />
+      <Header />
+        
         <div className="gymwearcontentobjects"> 
+        
+        <SideBar />
         <div className="gymwearcontent">
           {clothingList1.map((item) => (
             <div className="product-card product-grid__card  css-1xl2eyj" >
@@ -88,19 +411,7 @@ export const GymWear = () => {
           ))}
         </div>
 
-        <div className="gymwearcontent1">
-          {clothingList2.map((item) => (
-            <div className="product-card product-grid__card  css-1xl2eyj" >
-            <div key={item.id} className="clothing-item">
-              <img src={item.imageUrl} alt={item.name} className="clothing-image" />
-              <div className="clothing-details">
-                <h3 className="clothing-name">{item.name}</h3>
-                <p className="clothing-price">{item.price}</p>
-              </div>
-            </div>
-            </div>
-          ))}
-        </div>
+       
         </div>
       </div>
     </div>
--- src/GymWear/SideBar.js
+++ src/GymWear/SideBar.js
@@ -14,7 +14,72 @@ export const SideBar = () => {
           <h2>FILTER</h2>
         </div>
       </div>
-      
+      <div className="clothing">
+        <div className="option">
+          <h2>Clothing</h2>
+        </div>
+        <div className="option">
+          <input type="checkbox" id="tshirts" name="tshirts" />
+          <label htmlFor="tshirts">T-shirts</label>
+        </div>
+        <div className="option">
+          <input type="checkbox" id="trousers" name="trousers" />
+          <label htmlFor="trousers">Trousers</label>
+        </div>
+        <div className="option">
+          <input type="checkbox" id="tracksuits" name="tracksuits" />
+          <label htmlFor="tracksuits">Track-suits</label>
+        </div>
+        <div className="option">
+          <input type="checkbox" id="shorts" name="shorts" />
+          <label htmlFor="shorts">Shorts</label>
+        </div>
+      </div>
+      <div className="size">
+        <div className="option">
+          <h2>Size</h2>
+        </div>
+        <div className="option">
+          <input type="checkbox" id="size-s" name="size-s" />
+          <label htmlFor="size-s">Small</label>
+        </div>
+        <div className="option">
+          <input type="checkbox" id="size-m" name="size-m" />
+          <label htmlFor="size-m">Medium</label>
+        </div>
+        <div className="option">
+          <input type="checkbox" id="size-l" name="size-l" />
+          <label htmlFor="size-l">Large</label>
+        </div>
+        <div className="option">
+          <input type="checkbox" id="size-xl" name="size-xl" />
+          <label htmlFor="size-xl">Extra Large</label>
+        </div>
+      </div>
+      <div className="price">
+      <div className="option">
+          <h2>Price</h2>
+        </div>
+        <div className="option">
+            <input type="checkbox" id="price-below-500" name="price-below-500" />
+            <label htmlFor="price-below-500">Below 500</label>
+          </div>
+          <div className="option">
+            <input type="checkbox" id="price-above-1000" name="price-above-1000" />
+            <label htmlFor="price-above-1000">Above 1000</label>
+          </div>
+          <div className="option">
+            <input type="checkbox" id="price-above-1500" name="price-above-1500" />
+            <label htmlFor="price-above-1500">Above 1500</label>
+          </div>
+          <div className="option">
+            <input type="checkbox" id="price-above-2000" name="price-above-2000" />
+            <label htmlFor="price-above-2000">Above 2000</label>
+          </div>
+          <hr className="divider2" />
+        </div>
+
+
     </div>
   );
 };
